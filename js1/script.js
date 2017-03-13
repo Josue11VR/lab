50ce15c204a77eb6706d278dc2633d9466af6814
Josue11VR <josvr0511@gmail.com>
2017-03-13 14:44:54 -0600

    javascript 1

---
+++ js1/script.js
@@ -0,0 +1,9 @@
+var encapsular = function() {
+    alert('Boton 1');
+}
+
+var button = document.getElementById("button")
+
+button.addEventListener("click", function () {
+    alert('Boton 2');
+});
