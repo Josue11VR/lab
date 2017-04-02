11fbf6f1a511d61be100d34f12568ea7e4a3e801
Josue11VR <josvr0511@gmail.com>
2017-04-02 16:36:56 -0600

    parte3

--- js-jquery-bs/javascript/script.js
+++ js-jquery-bs/javascript/script.js
@@ -1,40 +1,40 @@
-var mostrarResultados = function(texto,estilo){
+var mostrarResultados = function (texto, estilo) {
 
-	$('#mostrarResultado').text(texto).attr('class', estilo);
+    $('#mostrarResultado').text(texto).attr('class', estilo);
 }
 
 
 
 $("#btn_calcular_edad").click(function () {
-	var nombre = $('#name').val();
-	var fecha = new Date($('#fecha_nacimiento').val()).getFullYear();
+    var nombre = $('#name').val();
+    var fecha = new Date($('#fecha_nacimiento').val()).getFullYear();
 
-	var user = new usuario(nombre,fecha);
-	user.presentarse();
+    var user = new usuario(nombre, fecha);
+    user.presentarse();
 });
 
 
 
 /*--------------- funcion constructiora -------------------------------*/
 
-var usuario = function usuario(nombre,fecha){
-	this.nombre = nombre;
-	this. fecha = fecha;
+var usuario = function usuario(nombre, fecha) {
+    this.nombre = nombre;
+    this.fecha = fecha;
 
 
 
 
-	function calcularEdad(){
-		var date = new Date().getFullYear();
+    function calcularEdad() {
+        var date = new Date().getFullYear();
 
-		return date-fecha;
-	}
+        return date - fecha;
+    }
 
-	this.presentarse = function(){
-		var edad = calcularEdad();
-		var mensaje = "Hola, mi nombre es "+this.nombre+" y tengo "+edad+" años."
-		mostrarResultados(mensaje,'naranja');
-	}
+    this.presentarse = function () {
+        var edad = calcularEdad();
+        var mensaje = "Hola, mi nombre es " + this.nombre + " y tengo " + edad + " años."
+        mostrarResultados(mensaje, 'naranja');
+    }
 }
 
 
@@ -42,34 +42,46 @@ var usuario = function usuario(nombre,fecha){
 
 $("#IDSelector").click(function () {
 
-      var texto = $('#thing1').text();
-      mostrarResultados(texto, 'azul');
+    var texto = $('#thing1').text();
+    mostrarResultados(texto, 'azul');
 });
 
 
 $("#ClassSelector").click(function () {
 
-      var texto = $('.special').text();
-      mostrarResultados(texto, 'rojo');
+    var texto = $('.special').text();
+    mostrarResultados(texto, 'rojo');
 });
 
 $("#TagSelector").click(function () {
 
-      var texto = $('body').text();
-      mostrarResultados(texto, 'verde');
+    var texto = $('body').text();
+    mostrarResultados(texto, 'verde');
 });
 
 $("#altercontent").click(function () {
 
-      var texto1 = $('h1').text();
-  
-      $('#thing1').append(texto1);
+    var texto1 = $('h1').text();
 
-      var texto2 = $('#thing1').text();
-     
-      mostrarResultados(texto2, 'amarillo');
+    $('#thing1').append(texto1);
+
+    var texto2 = $('#thing1').text();
+
+    mostrarResultados(texto2, 'amarillo');
 });
 
 $("#childDescendant").click(function () {
-	
-}
+
+    
+    var child = $('div p:first-child').first().text();
+    mostrarResultados(child, 'rosado');
+
+   
+
+    var descendant = $('#languages li').text();
+    
+    var union = child+descendant;
+    
+    mostrarResultados(union, 'rosado');
+
+});
